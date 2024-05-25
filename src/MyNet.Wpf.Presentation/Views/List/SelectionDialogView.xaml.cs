785e54c558ab9226cf3b8b47d50cbfccea209e00
Stéphane ANDRE <andre.cs2i@gmail.com>
2024-05-25 23:37:18 +0200

    feat: add export and import views (#4)
    * feat: add export views
    
    * fix: fix Binding property name
    
    * build: change version of Presentation
    
    * chore: update packages
    
    * refactor!: change Import management
    
    * fix sources binding
    
    * change size of window
    
    * update version
    
    * update packages

---
+++ src/MyNet.Wpf.Presentation/Views/List/SelectionDialogView.xaml.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Stéphane ANDRE. All Right Reserved.
+// See the LICENSE file in the project root for more information.
+
+namespace MyNet.Wpf.Presentation.Views.List
+{
+    /// <summary>
+    /// Interaction logic for CategoriesEditionView.xaml
+    /// </summary>
+    public partial class SelectionDialogView
+    {
+        public SelectionDialogView() => InitializeComponent();
+    }
+}
