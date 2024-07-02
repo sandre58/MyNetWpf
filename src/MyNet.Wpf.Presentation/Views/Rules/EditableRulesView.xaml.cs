872adfd475860c5b60318b55bfc571732a9c3372
Stéphane ANDRE <andre.cs2i@gmail.com>
2024-07-02 12:34:12 +0200

    feat: add editable rules view

---
+++ src/MyNet.Wpf.Presentation/Views/Rules/EditableRulesView.xaml.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Stéphane ANDRE. All Right Reserved.
+// See the LICENSE file in the project root for more information.
+
+using System.Windows;
+using MyNet.UI.Resources;
+
+namespace MyNet.Wpf.Presentation.Views.Rules
+{
+    public partial class EditableRulesView
+    {
+        public EditableRulesView() => InitializeComponent();
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register(
+                nameof(Header), typeof(string), typeof(EditableRulesView),
+                new PropertyMetadata(UiResources.AddRule));
+
+        public string Header
+        {
+            get => (string)GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+    }
+}
