00f9645c8bfacbb2e1f563a9654f9272d0ac85a8
Stéphane ANDRE (E104915) <stephane.andre_ext@michelin.com>
2024-09-23 22:58:18 +0200

    add showPageSIze for pagination

--- src/MyNet.Wpf.Presentation/Views/List/PaginationView.xaml.cs
+++ src/MyNet.Wpf.Presentation/Views/List/PaginationView.xaml.cs
@@ -1,10 +1,25 @@
 ﻿// Copyright (c) Stéphane ANDRE. All Right Reserved.
 // See the LICENSE file in the project root for more information.
 
+using System.Windows;
+
 namespace MyNet.Wpf.Presentation.Views.List
 {
     public partial class PaginationView
     {
         public PaginationView() => InitializeComponent();
+
+        #region ShowPageSize
+
+        public static readonly DependencyProperty ShowPageSizeProperty
+            = DependencyProperty.RegisterAttached(nameof(ShowPageSize), typeof(bool), typeof(PaginationView), new PropertyMetadata(true));
+
+        public bool ShowPageSize
+        {
+            get => (bool)GetValue(ShowPageSizeProperty);
+            set => SetValue(ShowPageSizeProperty, value);
+        }
+
+        #endregion
     }
 }
