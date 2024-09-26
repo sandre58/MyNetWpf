72a2a36929343d2211cae73901c04c3d4a263e8f
Stéphane ANDRE (E104915) <stephane.andre_ext@michelin.com>
2024-09-26 17:02:27 +0200

    add double to thichness converter H and V

--- src/MyNet.Wpf/Converters/DoubleToThicknessConverter.cs
+++ src/MyNet.Wpf/Converters/DoubleToThicknessConverter.cs
@@ -8,7 +8,7 @@ using System.Windows.Data;
 
 namespace MyNet.Wpf.Converters;
 
-public class DoubleThicknessToConverter : IValueConverter
+public class DoubleToThicknessConverter : IValueConverter
 {
     private enum Mode
     {
@@ -20,22 +20,30 @@ public class DoubleThicknessToConverter : IValueConverter
 
         Right,
 
-        Bottom
+        Bottom,
+
+        Horizontal,
+
+        Vertical,
     }
 
-    public static readonly DoubleThicknessToConverter All = new(Mode.All);
+    public static readonly DoubleToThicknessConverter All = new(Mode.All);
 
-    public static readonly DoubleThicknessToConverter Top = new(Mode.Top);
+    public static readonly DoubleToThicknessConverter Top = new(Mode.Top);
 
-    public static readonly DoubleThicknessToConverter Left = new(Mode.Left);
+    public static readonly DoubleToThicknessConverter Left = new(Mode.Left);
 
-    public static readonly DoubleThicknessToConverter Right = new(Mode.Right);
+    public static readonly DoubleToThicknessConverter Right = new(Mode.Right);
 
-    public static readonly DoubleThicknessToConverter Bottom = new(Mode.Bottom);
+    public static readonly DoubleToThicknessConverter Bottom = new(Mode.Bottom);
+
+    public static readonly DoubleToThicknessConverter Horizontal = new(Mode.Horizontal);
+
+    public static readonly DoubleToThicknessConverter Vertical = new(Mode.Vertical);
 
     private readonly Mode _mode;
 
-    private DoubleThicknessToConverter(Mode mode) => _mode = mode;
+    private DoubleToThicknessConverter(Mode mode) => _mode = mode;
 
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
@@ -46,6 +54,8 @@ public class DoubleThicknessToConverter : IValueConverter
             Mode.Right => new Thickness(0, 0, padding, 0),
             Mode.Top => new Thickness(0, padding, 0, 0),
             Mode.Bottom => new Thickness(0, 0, 0, padding),
+            Mode.Horizontal => new Thickness(padding, 0, padding, 0),
+            Mode.Vertical => new Thickness(0, padding, 0, padding),
             _ => new Thickness(padding)
         };
     }
